5f33af244e8e8c98a1d87fdd0ba1d5737f8797fe
koh-020 <97607232+koh-020@users.noreply.github.com>
2022-03-28 16:16:03 +0900

    update

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ $(".theTarget").skippr({
   // スライドショーの変化（"fade" or "slide"）
   transition : 'fade',
   // 変化にかかる時間（ミリ秒）
-  speed : 1000,
+  speed : 3000,
   // easingの種類
   easing : 'easeOutQuart',
   // ナビゲーションの形（"block" or "bubble"）
@@ -19,5 +19,5 @@ $(".theTarget").skippr({
   // キーボードの矢印キーによるスライド送りの設定（trueで有効）
   keyboardOnAlways : true,
   // 1枚目のスライド表示時に戻る矢印を表示するかどうか [false]:矢印を隠さない [true]:矢印を隠す
-  hidePrevious : false
+  hidePrevious : true
 });
